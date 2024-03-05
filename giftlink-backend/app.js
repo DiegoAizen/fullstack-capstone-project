63dbf36bd7a5c4760826cba7a678ec78f5b041d9
DiegoAizen <diegoxcorrea94@gmail.com>
2024-03-05 18:24:18 -0500

    Actualizar

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -13,22 +13,16 @@ app.use('/api/search', searchRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve static files for React App from a subdirectory
 app.use('/app', express.static(path.join(__dirname, 'public', 'react-app')));
 
-
-// Route for Home Page - Serve home.html as the default page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
-// Serve the React app's index.html for any other requests under /app
 app.get('/app/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'react-app', 'index.html'));
 });
 
-
-// Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
@@ -37,7 +31,6 @@ connectToDatabase().then(() => {
 
 app.use(express.json());
 
-// Route files
 const giftRoutes = require('./routes/giftRoutes');
 const authRoutes = require('./routes/authRoutes');
 const searchRoutes = require('./routes/searchRoutes');
@@ -46,17 +39,14 @@ const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
-// Use Routes
 app.use('/api/gifts', giftRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/search', searchRoutes);
 
-// Default Route for React App
 app.get('/app/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'react-app', 'index.html'));
 });
 
-// Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
