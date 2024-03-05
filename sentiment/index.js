e640ec5edbbaf561b35fbd0b9b81a0c6906037f5
DiegoAizen <diegoxcorrea94@gmail.com>
2024-03-05 18:36:13 -0500

    actualizar

--- sentiment/index.js
+++ sentiment/index.js
@@ -30,23 +30,19 @@ app.post('/sentiment', async (req, res) => {
 
         let sentiment = "neutral";
 
-        if (score < 0) {
+        if (analysisResult < 0) {
             sentiment = "negative";
-        } else if (score > 0) {
+        } else if (analysisResult > 0.33) {
             sentiment = "positive";
-        } else {
-            sentiment = "neutral";
         }
 
-        // Logging the result
         logger.info(`Sentiment analysis result: ${analysisResult}`);
 
-        // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment }
         res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment });
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
-        // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
-        //{{insert code here}}
+
+        res.status(500).json({ message: 'Error performing sentiment analysis' });
     }
 });
 
