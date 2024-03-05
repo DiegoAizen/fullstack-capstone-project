b99a8977879f1915914c479bb648ad79c98aceb0
Diego-Developer <109271780+DiegoAizen@users.noreply.github.com>
2024-03-05 18:40:04 -0500

    Update index.js

--- sentiment/index.js
+++ sentiment/index.js
@@ -37,18 +37,15 @@ app.post('/sentiment', async (req, res) => {
         }
 
         logger.info(`Sentiment analysis result: ${analysisResult}`);
-
+        
         res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment });
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
-<<<<<<< HEAD
 
-=======
-        // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
->>>>>>> 3635a6cc342a7d47f5b14a9a480e9a1f84c33ab9
         res.status(500).json({ message: 'Error performing sentiment analysis' });
     }
 });
+
 
 app.listen(port, () => {
     logger.info(`Server running on port ${port}`);
